37efdb1f21af580ad4ae7d2d0cdb54f2eaa0d84a
mahajanmalay <malaymahajan.mm@gmail.com>
2022-06-28 19:42:33 +0530

    first commit

---
+++ partials/_nav.php
@@ -0,0 +1,45 @@
+<?php
+if(isset($_SESSION['loggedin']) && $_SESSION['loggedin']==true){
+  $loggedin=true;
+}
+else{
+  $loggedin=false;
+}
+echo
+'<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="/LoginSystem">iSecure</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/LoginSystem/welcome.php">Home</a>
+        </li>';
+        if(!$loggedin){
+          echo'
+          <li class="nav-item">
+          <a class="nav-link" href="/LoginSystem/login.php">Login</a>
+          </li>
+          <li class="nav-item">
+          <a class="nav-link" href="/LoginSystem/signup.php">Signup</a>
+          </li>';
+        }
+        if($loggedin){
+          echo'
+          <li class="nav-item">
+          <a class="nav-link" href="/LoginSystem/logout.php">logout</a>
+          </li>';
+        }
+          
+      echo  
+      '</ul>
+      <form class="form-inline my-2 my-lg-0">
+        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>';
+?>
